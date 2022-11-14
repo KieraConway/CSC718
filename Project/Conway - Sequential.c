a17e2c6db1c82dbade283cee87325e8cf3f2546b
KieraConway <66643785+KieraConway@users.noreply.github.com>
2022-11-14 15:47:21 -0800

    Create Conway - Sequential.c
    Template, header, and program information only

---
+++ Project/Conway - Sequential.c
@@ -0,0 +1,60 @@
+/* ------------------------------------------------------------------------
+    Project - Sequential.c
+
+    CSC 718
+    Operating Systems & Parallel Programming
+    Dakota State University
+
+    Kiera Conway
+
+------------------------------------------------------------------------ */
+
+// // // // // // //
+// Program:
+//
+//	Sequential
+//
+//	parse a large text file, query the data, return the number of occurrences 
+//	for a prespecified string of characters
+//
+// // // // // // //
+
+
+/* Standard Libraries */
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/** ----------------------------- Structures ---------------------------- **/
+
+
+/** ----------------------------- Macros -------------------------------- **/
+
+
+/** ----------------------------- Constants ----------------------------- **/
+
+
+/** ----------------------------- Global -------------------------------- **/
+
+
+/** ----------------------------- Prototypes ---------------------------- **/
+
+
+/** ----------------------------- Functions ----------------------------- **/
+
+/*****************************************************
+ *
+ *  Function:  main()
+ *
+ *  Parameters:
+ *
+ *      argc - main parameter for argument count
+ *      argv - main parameter for argument values
+ *
+******************************************************/
+int main(int argc, char *argv[]) {
+
+
+
+}
+
