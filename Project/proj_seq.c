884d16d626a98a04e1f01d4f2f3ab16b03088357
KieraConway <66643785+KieraConway@users.noreply.github.com>
2022-12-01 14:08:08 -0800

    Formatting
    Updated formatting for proj_seq
    
    copy and pasted testFile a few times so that it is much longer

--- Project/proj_seq.c
+++ Project/proj_seq.c
@@ -28,8 +28,7 @@
 #include <getopt.h>
 #include <time.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+
 /** ----------------------------- Structures ---------------------------- **/
 
 
@@ -38,12 +37,11 @@
 
 /** ----------------------------- Constants ----------------------------- **/
 #define MAX_STR 100
-#define MAX_CHUNK 512
 
 /** ----------------------------- Global -------------------------------- **/
-int count;
-char fileName[MAX_STR];
-char targetStr[MAX_STR];
+int count;                      //occurrences of searchTerm in file
+char fileName[MAX_STR];         //file to search
+char searchTerm[MAX_STR];       //term to search for
 bool caseSensitive = false;     //flag: is search case-sensitive
 bool defaults = false;          //flag: use default settings (for testing)
 
@@ -73,8 +71,8 @@ int main(int argc, char *argv[]) {
      */
 
     /* Initialize Local Variables */
-    FILE* pInputFile;
-    char word[MAX_STR];
+    FILE* pInputFile;               //pointer to file
+    char word[MAX_STR];             //word currently being searched
 
     count = 0;                      //initialize counter
 
@@ -87,26 +85,15 @@ int main(int argc, char *argv[]) {
     ProcessArgs(argc,argv);
 
     /* Set Defaults */
-    if(defaults){               //if default flag set
+    if(defaults){               	//if defaults flag is set
 
         memcpy(fileName, "testFile.txt", strlen("testFile.txt")+1);
-        memcpy(targetStr, "life", strlen("life")+1);
+        memcpy(searchTerm, "life", strlen("life") + 1);
 
     }
 
-
-
-    //check if argc > 1
-        //fileName = argv[1]
-        //targetStr = argv[2]
-    // else Process Arguments
-
-    //set fileName and SearchString
-    //memcpy(fileName, "testFile.txt", strlen(src)+1);
-    //memcpy(targetStr, "life", strlen(src)+1);
-
-    //printf( "Searching for {} in {})
-    printf("\nSearching \'%s\' for \'%s\'...", fileName, targetStr);
+    /* Print Variables */
+    printf("\nSearching \'%s\' for \'%s\'...", fileName, searchTerm);
 
     /*
      * Open File
@@ -127,14 +114,14 @@ int main(int argc, char *argv[]) {
         TrimRight(word, "\t\n\v\f\r .,!?:;-""'");       //trim excess char (right)
         TrimLeft(word, "\t\n\v\f\r -");                 //trim excess char (left)
 
-        if(!caseSensitive){                                         //if not case-sensitive
-            ChangeToLower(word);                                //change to lowercase
-            ChangeToLower(targetStr);                           //change to lowercase
+        if(!caseSensitive){                             //if not case-sensitive
+            ChangeToLower(word);                        //change to lowercase
+            ChangeToLower(searchTerm);                  //change to lowercase
         }
 
         /* Compare to Target String */
-        if(strcmp(targetStr, word) == 0){                           //if strings match
-            count++;                                                //increment counter
+        if(strcmp(searchTerm, word) == 0){              //if strings match
+            count++;                                    //increment counter
         }
 
     }
@@ -147,18 +134,17 @@ int main(int argc, char *argv[]) {
     fclose(pInputFile);
 
     /* Print Results */
-    printf("\nThe word \'%s\' appears %d %s\n\n", targetStr,count, count == 1 ? "time" : "times");
+    printf("\nThe word \'%s\' appears %d %s\n\n", 
+           searchTerm, count, count == 1 ? "time" : "times");
 
     /* Calculate Runtime */
     clock_t end = clock();
     double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
     printf("\n<Program Runtime: %.4fs>\n\n", time_spent);
 
-
     return 0;
-
-
 }
+
 
 /* ------------------------------------------------------------------------
   Name -            Usage
@@ -177,15 +163,16 @@ void Usage(){
            "\t%-16s Specify File\t\tExample: proj_seq -f myFile.txt\n"
            "\t%-16s Specify Search Term\tExample: proj_seq -s \"term\"\n",
            "-h:","-d:", "-c:", "-f <file>:", "-s \"string\":");
-
 }
+
 
 /* ------------------------------------------------------------------------
   Name -            ProcessArgs
   Purpose -         Process user arguments
-  Parameters -      None todo!
+  Parameters -      argc:   number of arguments passed to program
+                    argv:   pointer to arguments passed to program
   Returns -         None
-  Side Effects -    None
+  Side Effects -    When necessary, updates global variables
   ----------------------------------------------------------------------- */
 void ProcessArgs(int argc, char ** argv){
 
@@ -194,10 +181,10 @@ void ProcessArgs(int argc, char ** argv){
     int input = 0;
     input = getopt( argc, argv,"dhcf:s:" );
 
-    /* No Arguments */
+    /* Handle if No Arguments */
     if (input == -1){
         Usage();            //print usage
-        exit(0);        //exit
+        exit(0);        	//exit
     }
 
     /* Process All Arguments */
@@ -209,6 +196,11 @@ void ProcessArgs(int argc, char ** argv){
                 defaults = true;
                 break;
 
+            /* Case Sensitive */
+            case 'c':
+                caseSensitive = true;
+                break;
+
             /* Specify File */
             case 'f':
                 memcpy(fileName, optarg, strlen(optarg)+1);
@@ -216,12 +208,7 @@ void ProcessArgs(int argc, char ** argv){
 
             /* Specify Target String */
             case 's':
-                memcpy(targetStr, optarg, strlen(optarg)+1);
-                break;
-
-            /* Case Sensitive */
-            case 'c':
-                caseSensitive = true;
+                memcpy(searchTerm, optarg, strlen(optarg) + 1);
                 break;
 
             /* Access Help Menu */
@@ -235,84 +222,116 @@ void ProcessArgs(int argc, char ** argv){
     }
 }
 
-/**************************************************
-*
-* TrimLeft()
-*
-*   Removes characters from the beginning of a string,
-*   defaults to whitespace charactersis trimChars
-*   is null
-*
-**************************************************/
+
+/* ------------------------------------------------------------------------
+  Name -            TrimLeft
+  Purpose -         Removes characters from the beginning of a string
+  Parameters -      str:         pointer to string to update
+                    trimChars:   characters to be removed [optional]
+  Returns -         Pointer to updated string
+  Side Effects -    none
+  ----------------------------------------------------------------------- */
 char* TrimLeft(char* str, const char* trimChars) {
+
+    /*
+     * Function Initialization
+     */
     int i;
     char* newStartingPos;
-    //int maxBufferSize = (strlen(str) + 1);	//took out, couldn't get gcc to work with strcpy_S
 
-    //check for these characters//
+    /*
+     * Set defaults if trimChars is null
+     */
     if (trimChars == NULL) {
         trimChars = "\t\n\v\f\r ";
     }
 
-    //identify variables//
+    /*
+     * Remove Specified Characters
+     */
+
+    /* Set Variables */
     i = 0;
     newStartingPos = str;
 
-    //loop each time above character shows, move start position once to the right//
+    /* Loop While Specified Character Shows */
     while (i < strlen(str) && strchr(trimChars, str[i]) != NULL) {
-        i++;
-        newStartingPos++;
+        i++;                		//increment index
+        newStartingPos++;   		//move start position once to the right
     }
 
-    //copy newStartingPos to str//
-    //strcpy_s(str, maxBufferSize, newStartingPos);	//wasn't working with gcc
+    /* Copy newStartingPos to str */
     strcpy(str, newStartingPos);
 
+    /* Return Updated String */
     return str;
 
 }
 
-/**************************************************
- *
- * TrimRight()
- *
- *  Removes characters from the end of a string,
- *  defaults to whitespace
- *  characters is trimChars is null.
- *
-**************************************************/
+
+/* ------------------------------------------------------------------------
+  Name -            TrimRight
+  Purpose -         Removes characters from the end of a string
+  Parameters -      str:         pointer to string to update
+                    trimChars:   characters to be removed [optional]
+  Returns -         Pointer to updated string
+  Side Effects -    none
+  ----------------------------------------------------------------------- */
 char* TrimRight(char* str, const char* trimChars) {
+
+    /*
+     * Function Initialization
+     */
     int i;
 
+    /*
+     * Set defaults if trimChars is null
+     */
     if (trimChars == NULL) {
         trimChars = "\t\n\v\f\r ";
     }
 
-    i = strlen(str) - 1;
+    /*
+     * Remove Specified Characters
+     */
+    i = strlen(str) - 1;				//set index to end of string
+
+    /* Loop While Specified Character Shows */
     while (i >= 0 && strchr(trimChars, str[i]) != NULL) {
-        str[i] = '\0';
-        i--;
+        str[i] = '\0';      			//replace character with null
+        i--;                			//decrement string
     }
+
+    /* Return Updated String */
     return str;
 }
 
-/**************************************************
- *
- * ChangeToLower
- *
- *  Changes the inputFile to uppercase and saves
- *  to ouputFile
- *
-**************************************************/
+
+/* ------------------------------------------------------------------------
+  Name -            ChangeToLower
+  Purpose -         Changes input string to lower case
+  Parameters -      str:         pointer to string to update
+  Returns -         Pointer to updated string
+  Side Effects -    none
+  ----------------------------------------------------------------------- */
 char* ChangeToLower(char* str) {
 
+    /*
+     * Function Initialization
+     */
     int i = 0;
 
-    /* read as long as there are characters to be read */
+
+    /*
+     * Loop Entire String
+     */
     while (i <= (strlen(str))) {
-        str[i] = tolower(str[i]);   //change to lowercase, overwrite letter
-        i++;
+        str[i] = tolower(str[i]);		//change to lowercase, overwrite letter
+        i++;                            //increment index
     }
+
+
+    /* Return Updated String */
     return str;
 
 }
