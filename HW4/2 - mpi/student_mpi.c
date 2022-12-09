230965a4ae8d5c9919590366154a1e1d057ddc6c
KieraConway <66643785+KieraConway@users.noreply.github.com>
2022-12-08 16:21:33 -0800

    Pre debug
    organized, pre-debug

--- HW4/2 - mpi/student_mpi.c
+++ HW4/2 - mpi/student_mpi.c
@@ -57,14 +57,11 @@ struct procInfo {
 #define MAX_ID 999999
 
 /** ----------------------------- Global -------------------------------- **/
-bool verbose = false;
 
 /** ----------------------------- Prototypes ---------------------------- **/
 bool ContainsConsecutiveValues(int studentID);
 bool ComputesInvalidSum(int studentID);
 void FindRange(pProc p, int max, int numProcs);
-void ProcessArgs(int argc, char ** argv);
-void Usage();
 /** ----------------------------- Functions ----------------------------- **/
 
 /*****************************************************
@@ -96,10 +93,10 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);		//Determines the rank (PID) of the calling process
     MPI_Comm_size(MPI_COMM_WORLD, &numProcs);	//Determines size of the processor group associated
 
-    if(verbose){
-        printf("Process %d starting... \n", rank);
-    }
-    
+
+    printf("Process %d starting... \n", rank);
+    fflush(stdout);
+
     /* Initialize Process Information */
     proc process;
 
@@ -112,9 +109,10 @@ int main(int argc, char *argv[]) {
     /*** *** *** *** *** *** ***
      *     Find Valid SIDs
      *** *** *** *** *** *** ***/
-    if(verbose){
-        printf("Process %d searching sid range: %d-%d \n", rank, process.start, process.end);
-    }
+
+    printf("Process %d searching sid range: %d-%d \n", rank, process.start, process.end);
+    fflush(stdout);
+
     for(int localSID = process.start; localSID <= process.end; localSID++){
 
         if(localSID > 100000){
@@ -123,9 +121,9 @@ int main(int argc, char *argv[]) {
             }
         }
     }
-    if(verbose){
-        printf("Process %d found %d valid SIDs \n", rank, localCount);
-    }
+
+    printf("Process %d found %d valid SIDs \n", rank, localCount);
+    fflush(stdout);
 
     /*** *** *** *** *** *** ***
      * Terminate Parallelization
@@ -250,71 +248,3 @@ void FindRange(pProc p, int max, int numProcs){
 
     printf("%d: %d-%d\n", p->pid, p->start, p->end);
 }
-
-
-/* ------------------------------------------------------------------------
-  Name -            ProcessArgs
-  Purpose -         Process user arguments
-  Parameters -      argc:   number of arguments passed to program
-                    argv:   pointer to arguments passed to program
-  Returns -         None
-  Side Effects -    When necessary, updates global variables
-  ----------------------------------------------------------------------- */
-void ProcessArgs(int argc, char ** argv){
-
-    /*** *** *** *** *** *** ***
-      *     Process Arguments
-      *** *** *** *** *** *** ***/
-    /* Get Input */
-    int input = 0;
-    int threads = 0;
-
-    input = getopt( argc, argv,"hvt:" );
-
-    /* Handle if No Arguments */
-    if (input == -1){
-        printf("Note: Defaults Set [No Arguments Given]\n\n");
-        fflush(stdout);
-        return;
-    }
-
-    /* Process All Arguments */
-    while (input != -1) {
-        switch (input) {
-
-            /* Verbose Mode */
-            case 'v':
-                verbose = true;
-                break;
-
-            /* Access Help Menu */
-            case 'h':
-            default:
-                Usage();
-                exit(0);
-        }
-
-        input = getopt( argc, argv, "hvt:" );
-    }
-}
-
-
-/* ------------------------------------------------------------------------
-  Name -            Usage
-  Purpose -         Prints Help Menu
-  Parameters -      None
-  Returns -         None
-  Side Effects -    None
-  ----------------------------------------------------------------------- */
-void Usage(){
-
-    printf("\nAn MPI Program for counting 'acceptable' unique student id numbers\n"
-           "ver 1.0, 2022\n\n"
-           "Usage: student_mpi -h -v\n\n"
-           "\t%-14s %-24s %-16s\n\n"
-           "\t%-14s %-24s %-8s %-12s %-3s %-5s\n",
-
-           "-h:", "Display Usage summary", "Example: proj_seq -h",
-           "-v:", "Set Verbose Mode", "Default:", verbose == true ? "True" : "False", "|", "Example: proj_seq -v");
-}
-
