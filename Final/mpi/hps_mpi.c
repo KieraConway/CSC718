9f2dae47b935f102df439c19d2535d888790bf2f
KieraConway <66643785+KieraConway@users.noreply.github.com>
2022-12-14 01:04:28 -0800

    Update hps_mpi.c
    First draft of hps_mpi

--- Final/mpi/hps_mpi.c
+++ Final/mpi/hps_mpi.c
@@ -21,24 +21,60 @@ For example S(7) = 2.592857142857 to 12 digits of precison.
 
 */
 
+/* Standard Libraries */
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+#include <mpi.h>			// Message Passing Interface
 
+/** ----------------------------- Structures ---------------------------- **/
+typedef struct procInfo proc;
+typedef struct procInfo * pProc;
+
+struct procInfo {
+    int pid;				//process rank/pid
+    int start;				//process start
+    int end;				//process end
+};
+/** ----------------------------- Macros -------------------------------- **/
+/*
+ * Function Keys
+ *      i : pid/rank
+ *      n : max value
+ *      p : total processes
+ */
+#define FIND_START(i,n,p)	floor((i*n)/(p))
+#define FIND_END(i,n,p)     (floor((i+1)*(n)/(p)))-1
+/** ----------------------------- Prototypes ---------------------------- **/
+void FindRange(pProc p, int max, int numProcs);
+
+/** ----------------------------- Functions ----------------------------- **/
+
+/*****************************************************
+ *
+ *  Function:  main()
+ *
+ *  Parameters:
+ *
+ *      argc - main parameter for argument count
+ *      argv - main parameter for argument values
+ *
+******************************************************/
 int main(int argc, const char **argv)
 {
 
     /*** *** *** *** *** *** ***
     *  Program Initialization
     *** *** *** *** *** *** ***/
-
     /* Initialize Local Variables */
-    long unsigned int *digits;
+    long unsigned int *localDigits;
+    long unsigned int *globalDigits;
     long unsigned int remainder, div, mod;
-    int i, digit;
+    int i, localDigitIndex, globalDigitIndex;
 
-    /* Save Program Start Time */
-    clock_t start = clock();
+    double elapsed_time;
+    int numProcs, rank;
 
     /*** *** *** *** *** *** ***
      *    Parse User Input
@@ -55,85 +91,145 @@ int main(int argc, const char **argv)
 
     /* Save Input */
     int n = atoi((char*)argv[1]);	// value of N
-    int d = atoi((char*)argv[2]);	// numbers of digits
+    int d = atoi((char*)argv[2]);	// numbers of localDigits
 
-
-    /*** *** *** *** *** *** ***
-     * Allocate Digits Variable
-     *** *** *** *** *** *** ***/
-    /* Allocate Memory for Digits */
-    digits = (long unsigned int *)malloc(sizeof(long unsigned int) * (d+11));
-    if (!digits)
+    /* Allocate Memory for Global Digits */
+    localDigits = (long unsigned int *)malloc(sizeof(long unsigned int) * (d + 11));
+    if (!localDigits)
     {
         printf("memory allocation failed\n");
         exit(-1);
     }
 
-    /* Init Digits to 0 */
-    for (digit = 0; digit < d + 11; ++digit)
-        digits[digit] = 0;
+    /*** *** *** *** *** *** ***
+    *  Process Initialization
+    *** *** *** *** *** *** ***/
+    /* Parallelization Init */
+    MPI_Init(&argc, &argv);			//Initializes the MPI execution environment
+    MPI_Barrier (MPI_COMM_WORLD);	//Blocks caller until all processes in the communicator have called it
+
+    elapsed_time = - MPI_Wtime();
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);		//Determines the rank (PID) of the calling process
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcs);	//Determines size of the processor group associated
+
+    printf("Process %d starting... \n", rank);
+    fflush(stdout);
+
+    /* Initialize Process Information */
+    proc process;
+
+    process.start = process.end = 0;
+    process.pid = rank;
+
+    /* Determine Process Section */
+    FindRange(&process, n, numProcs);
+
+
+    /*** *** *** *** *** *** *** *** ***
+     *  Allocate Local Digits Variable
+     *** *** *** *** *** *** *** *** ***/
+    /* Allocate Memory for Local Digits */
+    localDigits = (long unsigned int *)malloc(sizeof(long unsigned int) * (d + 11));
+    if (!localDigits)
+    {
+        printf("Process %d Error: memory allocation failed\n", process.pid);
+        exit(-1);
+    }
+
+    /* Init Local Digits to 0 */
+    for (localDigitIndex = 0; localDigitIndex < d + 11; ++localDigitIndex)
+        localDigits[localDigitIndex] = 0;
 
     /*** *** *** *** *** *** ***
      *      Calculate HPS
      *** *** *** *** *** *** ***/
-    /* Calculate HPS saving each digit in digits[i] */
-    for (i = 1; i <= n; ++i)
+    /* Calculate HPS saving each localDigitIndex in localDigits[i] */
+    for (i = process.start; i <= process.end; ++i)
     {
         remainder = 1;
-        for (digit = 0; digit < d + 11 && remainder; ++digit) 	{
+        for (localDigitIndex = 0; localDigitIndex < d + 11 && remainder; ++localDigitIndex) 	{
             div = remainder / i;
             mod = remainder % i;
-            digits[digit] += div;
+            localDigits[localDigitIndex] += div;
             remainder = mod * 10;
         }
     }
 
     /* step r1:
      *      init regrouping, from d+11-1 to 1
-     *      regroup any digits[i] > 10
+     *      regroup any localDigits[i] > 10
      */
     for (i = d + 11 - 1; i > 0; --i)
     {
-        digits[i - 1] += digits[i] / 10;
-        digits[i] %= 10;
+        localDigits[i - 1] += localDigits[i] / 10;
+        localDigits[i] %= 10;
     }
 
-    /* step r2:
-     *      skip all the digits after d
-     *      round if digits[i] > 5
-     */
-    if (digits[d + 1] >= 5)
+    //todo: REMOVE BELOW
+    /* Print Digits Left of Decimal */
+    printf("%d[%d]: %ld.", process.pid, "0", localDigits[0]);
+
+    /* Print Digits Right of Decimal */
+    for (i = 1; i <= d; ++i)
     {
-        ++digits[d];
+        printf("%d[%d]: %ld", process.pid, i, localDigits[i]);
+    }
+    printf("\n");
+    //todo: end Remove
+
+    /*** *** *** *** *** *** ***
+     * Terminate Parallelization
+     *** *** *** *** *** *** ***/
+    MPI_Reduce (&localDigits,
+                &globalDigits,
+                1,
+                MPI_INT,
+                MPI_SUM,
+                0,
+                MPI_COMM_WORLD);				//Reduces values on all processes within a group
+    elapsed_time += MPI_Wtime();				//Calculate elapsed time
+
+    MPI_Finalize();								//Terminates MPI execution environment
+
+
+    /* step r2:
+     *      skip all the localDigits after d
+     *      round if localDigits[i] > 5
+     */
+    if (globalDigits[d + 1] >= 5)
+    {
+        ++globalDigits[d];
     }
 
     /* step r3:
      *      init regrouping again because of r2
-     *      regroup any digits[i] > 10
+     *      regroup any localDigits[i] > 10
      */
     for (i = d; i > 0; --i)
     {
-        digits[i - 1] += digits[i] / 10;
-        digits[i] %= 10;
+        globalDigits[i - 1] += globalDigits[i] / 10;
+        globalDigits[i] %= 10;
     }
 
     /*** *** *** *** *** *** ***
      *      Print Sum
      *** *** *** *** *** *** ***/
     /* Print Digits Left of Decimal */
-    printf("%ld.", digits[0]);
+    printf("%ld.", globalDigits[0]);
 
     /* Print Digits Right of Decimal */
     for (i = 1; i <= d; ++i)
     {
-        printf("%ld",digits[i]);
+        printf("%ld", globalDigits[i]);
     }
     printf("\n");
 
     /*** *** *** *** *** *** ***
      *Deallocate Digits Variable
      *** *** *** *** *** *** ***/
-    free(digits);
+    free(localDigits);
+    free(globalDigits);
 
     /*** *** *** *** *** *** ***
     *  Program Termination
@@ -147,4 +243,22 @@ int main(int argc, const char **argv)
            "<","Program Runtime:", programTime, ">");
 
     return 0;
+}
+
+/* ------------------------------------------------------------------------
+  Name -            FindRange
+  Purpose -         Finds range for process using block allocation
+  Parameters -      p:		  pointer to process structure
+					max:	  range max (n)
+					numProcs: number of process to split between
+  Returns -         None: data is placed in process pointed to by p
+  Side Effects -    Process structure is updated
+  ----------------------------------------------------------------------- */
+void FindRange(pProc p, int max, int numProcs){
+
+    p->start = FIND_START(p->pid, max, numProcs) + (double) 1;
+    p->end = FIND_END(p->pid, max, numProcs)+ (double) 1;
+
+    printf("Process %d searching SID range %d-%d\n", p->pid, p->start, p->end);
+    fflush(stdout);
 }
